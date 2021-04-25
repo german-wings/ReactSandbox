469e31d1af28eea4ffd32e4c9295366126849739
german-wings <52790214+german-wings@users.noreply.github.com>
2021-04-25 15:56:06 +0530

    Update mortal_kombat.js

--- mortal_kombat.js
+++ mortal_kombat.js
@@ -2,8 +2,37 @@
     {
         "name" : "SubZero",
         "orgin" : "EarthRealm",
-        "allies" :"Ice Sword",
+        "ability" :"Freezing Ice Sword",
         "xp" : "930XP",
         "about" : "Sub-Zero, once known as Bi-Han, is a Cryomancer in service as an assassin of the Lin Kuei clan. He serves at their command, his deadly training and cunning intelligence making him a ruthless opponent who cuts down those in his path with his ice attacks. At some point, he acted as a rival assassin to Hanzo Hasashi, Scorpion, banishing him to the Netherrealm. Under the orders of Shang Tsung, he seeks to eliminate Cole Young before the upcoming fighter can discover his arcana and be trained in the ways of Mortal Kombat."
+    },
+    {
+        "name" : "Scorpion",
+        "orgin" : "EarthRealm",
+        "ability" :"Deadly Dagger",
+        "xp" : "1030XP",
+        "about" : "Scorpion aka Hanzo Hasashi, renowned as the greatest warrior ever to live, was a ninja born of the Shirai Ryu clan. His death at the hands of rival assassin Bi-Han banished him to the Netherrealm, his soul trapped in eternal fire."
+    },
+    {
+        "name" : "Raiden",
+        "orgin" : "The Heavens",
+        "ability" :"Lighting Thunder",
+        "xp" : "20000XP",
+        "about" : "An Elder God and the champion of Earthrealm, Lord Raiden grants sanctuary in his temple to those who bear the dragon mark that serves as an invitation to Mortal Kombat. He has assembled a team of champions to battle against the forces of Outworld, but has begun to lose faith in the world because people have forgotten its mythology. Having lost in the previous nine tournaments, he does not have much hope for his latest group, and is beginning to think that perhaps he should give up. He has the power to send anyone within Earthrealm to somewhere else within Earthrealm with a single flash of lightning."
+    },
+    {
+        "name" : "Kung Lao",
+        "orgin" : "EarthRealm",
+        "ability" :"Barbers Razor-brimmed hat",
+        "xp" : "10000XP",
+        "about" : "A descendant of The Great Kung Lao, Kung Lao is one of Lord Raiden's most respected warriors. His razor-brimmed hat is his primary weapon, which he uses to great effect via teleportation. Somewhat mysterious in nature, he does not talk much. He works with other champions of Earthrealm to train Cole Young in Lord Raiden's temple in order to prepare him for Mortal Kombat against the enemies from Outworld."
+    },
+    {
+        "name" : "Kotal Kahn",
+        "orgin" : "Osh-Tekk",
+        "ability" :"Soul Scratch",
+        "xp" : "1500XP",
+        "about" : "Kotal Kahn is a warrior from the realm of Osh-Tekk, which was once ruled by Kotal K'etz, his father, but was later merged into Outworld by conquest. After the death of his father, he became the last of his kind. He would eventually usurp Mileena as emperor of Outworld due to his popularity and charisma.Unlike previous Outworld emperors, Kotal Kahn is uninterested in conquering other realms, preferring instead to focus on internal affairs and stabilizing the realm. For this reason, he is strongly opposed to liberating realms that are already under Outworld's domain, believing that a united Outworld would prove stronger against its enemies. He also bears no hatred for Earthrealm or its inhabitants, and has willingly cooperated with them on several occasions, although he is shown to be distrustful of Raiden.At some point in history, he journeyed throughout Earthrealm and encountered the Mayan civilization, wherein he came to be considered a hero after driving off invading Spanish conquistadors. The Mayans named him Buluc Chabtan, their god of war. In his honor, the Mayans began emulating his penchant for consuming blood. However, this caused the Mayans to contract blood disease, killing thousands. In horror and shame, Kotal left the Mayans and returned to Outworld."
     }
+
 ]
