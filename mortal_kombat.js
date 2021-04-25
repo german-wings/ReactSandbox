2e5a184bc997b1a793a98cc408d6706250182964
german-wings <52790214+german-wings@users.noreply.github.com>
2021-04-25 11:31:11 +0530

    Create mortal_kombat.js

---
+++ mortal_kombat.js
@@ -0,0 +1,12 @@
+[
+    {
+        name : 'Scorpio',
+        ability : ['fire','rope'],
+        special_attack : ['lungs ripper' , 'fry the meat']
+    },
+    {
+        name :'Sub Zero',
+        ability : ['freeze','blade'],
+        special_attack : ['ice blade' , 'deep freeze']
+    }
+]
