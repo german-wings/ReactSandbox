84c80ba811c0de855bdd6399b5150b111f320ccb
german-wings <52790214+german-wings@users.noreply.github.com>
2021-04-25 11:45:48 +0530

    Update mortal_kombat.js
    Valid Json added

--- mortal_kombat.js
+++ mortal_kombat.js
@@ -1,12 +1,12 @@
 [
     {
-        name : 'Scorpio',
-        ability : ['fire','rope'],
-        special_attack : ['lungs ripper' , 'fry the meat']
+        "name" : "Scorpio",
+        "ability" : ["fire","rope"],
+        "special_attack" : ["lungs ripper" , "fry the meat"]
     },
     {
-        name :'Sub Zero',
-        ability : ['freeze','blade'],
-        special_attack : ['ice blade' , 'deep freeze']
+        "name" :"Sub Zero",
+        "ability" : ["freeze","blade"],
+        "special_attack" : ["ice blade" , "deep freeze"]
     }
 ]
