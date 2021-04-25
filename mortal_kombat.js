0f498450b6ac236e70357481cf0109ddcb61dabe
german-wings <52790214+german-wings@users.noreply.github.com>
2021-04-25 15:34:33 +0530

    Update mortal_kombat.js

--- mortal_kombat.js
+++ mortal_kombat.js
@@ -1,12 +1,9 @@
 [
     {
-        "name" : "Scorpio",
-        "ability" : ["fire","rope"],
-        "special_attack" : ["lungs ripper" , "fry the meat"]
-    },
-    {
-        "name" :"Sub Zero",
-        "ability" : ["freeze","blade"],
-        "special_attack" : ["ice blade" , "deep freeze"]
+        "name" : "SubZero",
+        "orgin" : "EarthRealm",
+        "allies" :"Ice Sword",
+        "xp" : "930XP",
+        "about" : "Sub-Zero, once known as Bi-Han, is a Cryomancer in service as an assassin of the Lin Kuei clan. He serves at their command, his deadly training and cunning intelligence making him a ruthless opponent who cuts down those in his path with his ice attacks. At some point, he acted as a rival assassin to Hanzo Hasashi, Scorpion, banishing him to the Netherrealm. Under the orders of Shang Tsung, he seeks to eliminate Cole Young before the upcoming fighter can discover his arcana and be trained in the ways of Mortal Kombat."
     }
 ]
